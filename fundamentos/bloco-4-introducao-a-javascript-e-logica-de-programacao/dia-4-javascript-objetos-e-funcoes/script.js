d202e12b83e243e9d93a079d28d06b349608aac5
RafaelOliveiraElias <rafaelias567@gmail.com>
2021-11-30 23:38:48 -0300

    Parte 1 Objetos for/in

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,38 @@
+//exercicios 1 ao 5
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+console.log("Bem vinda, " + info.personagem);
+
+info['recorrente'] = 'Sim'
+
+console.log(info.recorrente)
+
+console.table(info)
+
+for (let key in info){
+    console.log(key)
+};
+
+
+for (let key in info){
+    console.log(info[key])
+}
+
+
+let novo = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+
+for (let key in info){
+    console.log(info[key] + " e " + novo[key])
+}
+
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -0,0 +1,24 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  console.log("O livro favorito de " + leitor.nome + " se chama '" + leitor.livrosFavoritos[0]['titulo'] + "'")
+
+  leitor.livrosFavoritos[1] = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco', 
+  }
+
+  console.log(leitor.livrosFavoritos)
+
+  console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos")
