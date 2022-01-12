1d32d316be44b7ada6e8b62f12713d91a4069ed7
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-01-12 20:04:00 -0300

    ainda tem que terminar

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/part1.js
@@ -0,0 +1,19 @@
+const testingScope = (escopo) => {
+    if (escopo === true) {
+        
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope}! ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+
+  testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  // Seu código aqui.
+  
+  console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/part2.js
@@ -0,0 +1,2 @@
+const factorial = number => (number === 0) ? 1 : number * factorial(number -1)
+
