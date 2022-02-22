df9f2447845782f2a83a9216425720d338dc15c9
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-02-22 15:27:43 -0300

    going to children parents infos

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/aula/exercaula1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import React from 'react';
+import Forms from './Forms';
+import './App.css';
+
+class App extends React.Component {
+  render() { 
+    return (<Forms />);
+  }
+}
+
+export default App;
---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/aula/exercaula1/src/Forms.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor() {
+        super();
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+        estadoFavorito: '',
+        idade: 0,
+        vaiComparecer: false,
+        };
+    }
+    handleChange({ target }) {
+        const { name } = target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;      
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+                <form className="form">
+                    <label>
+                        Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
+                        <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+                    </label>
+                    <input
+                    type="number"
+                    name="idade"
+                    value={this.state.idade} onChange={this.handleChange}
+                    />
+                    <input type="file" ref={this.fileInput} />
+                    <input
+                    type="checkbox"
+                    name="vaiComparecer"
+                    value={this.state.vaiComparecer} onChange={this.handleChange}
+                    />
+                </form>
+            </div>
+    );
+    }
+}
+
+export default Form;
